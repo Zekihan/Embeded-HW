0f4cc79bcecfa47f2fef10c520edcf36d85fbe0d
Zekihan <zekihanazman@std.iyte.edu.tr>
2020-06-12 16:03:09 +0300

    bash script for otomat

--- embeded-hw3/horner_poly_unop.c
+++ embeded-hw3/horner_poly_unop.c
@@ -2,18 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 
-/*Direct polynomial evaluation*/
-double poly(double a[], double x, int degree){
- long int i;
- double result = a[0];
- double xpwr = x;
- for(i=1; i<=degree;i++){
-	result += a[i]* xpwr;
-	xpwr = x* xpwr;
- }
- return result;
-}
-
 /*Horner's method for polynomial evaluation*/
 double horner_poly(double a[], double x, int degree){
  long int i;
@@ -37,7 +25,6 @@ void main(){
  
  clock_t time = clock(); //starts time 
  for(int i = 0; i < 1000000; i++)//call multiple times to get significant values
- 	//poly(a,2,999);
  	horner_poly(a,2,999);
  time = clock() - time; //ends time
 
