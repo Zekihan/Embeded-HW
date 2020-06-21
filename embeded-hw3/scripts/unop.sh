b245f82fa0fe72917d5ac4002b56185d12e94df5
Zekihan <zekihanazman@std.iyte.edu.tr>
2020-06-21 18:37:39 +0300

    optimization for poly & mixed method added

--- embeded-hw3/scripts/unop.sh
+++ embeded-hw3/scripts/unop.sh
@@ -3,24 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-gcc ../source/horner_poly_unop.c -o ../out/horner_poly_unop_out
-gcc ../source/horner_poly_op1.c -o ../out/horner_poly_op1_out
-gcc ../source/poly_unop.c -o ../out/poly_unop_out
+gcc ../source/pp.c -o ../out/pp
 
-echo Horner Poly unoptimized
 for i in {1..10}
 do
-    ../out/horner_poly_unop_out
-done
-
-echo Horner Poly optimized 1
-for i in {1..10}
-do
-../out/horner_poly_op1_out
-done
-
-echo Poly Unoptimized
-for i in {1..10}
-do
-    ../out/poly_unop_out
+    echo $i
+    ../out/pp
 done
--- embeded-hw3/source/pp.c
+++ embeded-hw3/source/pp.c
@@ -16,6 +16,32 @@ double poly(double a[], double x, int degree)
 	return result;
 }
 
+/*Direct polynomial evaluation*/
+double op_poly(double a[], double x, int degree)
+{
+	int i;
+	int limit = degree - 1;
+	double result1 = a[0];
+	double result2 = 0;
+	double xpwr1 = x;
+	double xpwr2 = x * x;
+	for (i = 1; i <= limit; i += 2)
+	{
+		result1 += a[i] * xpwr1;
+		xpwr1 = x * x * xpwr1;
+
+		result2 += a[i + 1] * xpwr2;
+		xpwr2 = x * x * xpwr2;
+	}
+
+	for (; i <= degree; i++)
+	{
+		result1 += a[i] * xpwr1;
+		xpwr1 = x * x * xpwr1;
+	}
+	return result1 + result2;
+}
+
 /*Horner's method for polynomial evaluation*/
 double horner_poly(double a[], double x, int degree)
 {
@@ -23,6 +49,62 @@ double horner_poly(double a[], double x, int degree)
 	double result = a[degree];
 	for (i = degree - 1; i >= 0; i--)
 		result = a[i] + (x * result);
+	return result;
+}
+
+/*Horner's method for polynomial evaluation*/
+double op_horner_poly(double a[], double x, int degree)
+{
+	int i = degree - 1;
+	double result = a[degree];
+	for (; i >= 2; i -= 2)
+	{
+		result = a[i] + (x * result);
+		result = a[i - 1] + (x * result);
+	}
+	for (; i >= 0; i--)
+	{
+		result = a[i] + (x * result);
+	}
+	return result;
+}
+
+/*Combined for polynomial evaluation*/
+double combined(double a[], double x, int degree)
+{
+	long int i;
+	double result = a[0];
+	double xpwr = x;
+	for (i = 1; i <= degree - 1; i += 2)
+	{
+		result += xpwr * (a[i] + (a[i + 1] * x));
+		xpwr = x * x * xpwr;
+	}
+	for (; i <= degree; i++)
+	{
+		result += xpwr * (a[i]);
+		xpwr = x * xpwr;
+	}
+	return result;
+}
+
+/*Combined for polynomial evaluation*/
+double op_combined(double a[], double x, int degree)
+{
+	int i;
+	int limit = degree - 1;
+	double result = a[0];
+	double xpwr = x;
+	for (i = 1; i <= limit; i += 2)
+	{
+		result += xpwr * (a[i] + (a[i + 1] * x));
+		xpwr = x * x * xpwr;
+	}
+	for (; i <= degree; i++)
+	{
+		result += xpwr * (a[i]);
+		xpwr = x * xpwr;
+	}
 	return result;
 }
 
@@ -38,17 +120,55 @@ void main()
 		//printf("%f\n", a[i]);
 	}
 
-	clock_t time = clock();			  //starts time
+	int deg = 10;
+	printf("%f\n", combined(a, 2, deg));
+	printf("%f\n", op_combined(a, 2, deg));
+	printf("%f\n", poly(a, 2, deg));
+	printf("%f\n", op_poly(a, 2, deg));
+	printf("%f\n", horner_poly(a, 2, deg));
+	printf("%f\n", op_horner_poly(a, 2, deg));
+
+	clock_t time;
+
+	time = clock();					  //starts time
 	for (int i = 0; i < 1000000; i++) //call multiple times to get significant values
 		//poly(a,2,999);
-		horner_poly(a, 2, 999);
+		combined(a, 2, 999);
 	time = clock() - time; //ends time
+	printf("combined: %f\n", (double)time / CLOCKS_PER_SEC);
 
-	clock_t time = clock();			  //starts time
+	time = clock();					  //starts time
+	for (int i = 0; i < 1000000; i++) //call multiple times to get significant values
+		//poly(a,2,999);
+		op_combined(a, 2, 999);
+	time = clock() - time; //ends time
+	printf("combined op: %f\n", (double)time / CLOCKS_PER_SEC);
+
+	time = clock();					  //starts time
 	for (int i = 0; i < 1000000; i++) //call multiple times to get significant values
 		//poly(a,2,999);
 		poly(a, 2, 999);
 	time = clock() - time; //ends time
+	printf("poly: %f\n", (double)time / CLOCKS_PER_SEC);
 
-	printf("%f\n", (double)time / CLOCKS_PER_SEC);
+	time = clock();					  //starts time
+	for (int i = 0; i < 1000000; i++) //call multiple times to get significant values
+		//poly(a,2,999);
+		op_poly(a, 2, 999);
+	time = clock() - time; //ends time
+	printf("poly op: %f\n", (double)time / CLOCKS_PER_SEC);
+
+	time = clock();					  //starts time
+	for (int i = 0; i < 1000000; i++) //call multiple times to get significant values
+		//poly(a,2,999);
+		horner_poly(a, 2, 999);
+	time = clock() - time; //ends time
+	printf("horner: %f\n", (double)time / CLOCKS_PER_SEC);
+
+	time = clock();					  //starts time
+	for (int i = 0; i < 1000000; i++) //call multiple times to get significant values
+		//poly(a,2,999);
+		op_horner_poly(a, 2, 999);
+	time = clock() - time; //ends time
+	printf("horner op: %f\n", (double)time / CLOCKS_PER_SEC);
 }
